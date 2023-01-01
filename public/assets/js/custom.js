52af1f0585dcc0927bb8f2316e2fc9aa33a57129
Isuru Ranawaka <e184439@esoft.academy>
2023-01-01 16:01:14 +0530

    changed project structure added xhr functions to get and post

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,3 +1,41 @@
 // API url
-var api_base_url = "https://cb60dbea-5ecc-4465-aba7-12bd894f8e06.mock.pstmn.io/api";
+var api_base_url = "http://powerfuel.janaksystem.com/api";
 var api_key = '';
+
+function sendGetRequest(route, data, headers, onSuccess, onError) {
+    return $.ajax({
+        url: api_base_url + route,
+        method: 'GET',
+        data: data,
+        headers: headers,
+        success: function (data) {
+            if(typeof onSuccess === 'function') {
+                onSuccess(data);
+            }
+        },
+        error: function (data) {
+            if(typeof onError === 'function') {
+                onError(data);
+            }
+        }
+    });
+}
+
+function sendPostRequest(route, data, headers, onSuccess, onError) {
+    return $.ajax({
+        url: api_base_url + route,
+        method: 'POST',
+        data: data,
+        headers: headers,
+        success: function (data) {
+            if(typeof onSuccess === 'function') {
+                onSuccess(data);
+            }
+        },
+        error: function (data) {
+            if(typeof onError === 'function') {
+                onError(data);
+            }
+        }
+    });
+}
